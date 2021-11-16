2ebff562cb66ca211cb4071c9b8b3e9c0b60ba4e
Baraa Chiban <baraashaiban@gmail.com>
2021-11-16 11:58:49 +0100

    working site

--- PHP _DAY5_RES/index.php
+++ PHP _DAY5_RES/index.php
@@ -4,26 +4,28 @@ require_once "actions/db_connect.php";
 $sql = "SELECT * FROM products";
 $result = mysqli_query($connect, $sql);
 $tbody = "";
+$button= "";
+$button2 = "";
 
-$class= "hide";
-$class2 = "hide";
-if(isset($_SESSION["adm"])){
+/* $class= "hide";
+$class2 = "hide"; */
+/* if(isset($_SESSION["adm"])){
   $class="show";
   $class2="show";
-} 
+}  */
 
   
 if (mysqli_num_rows($result) > 0) {
  
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
       
-      // if(isset($_SESSION['adm'])){
-      // $button = '<a href="update.php?id=' .$row['id'].'"><button class="btn btn-primary btn-sm m-2" type="button">Edit</button></a><a href="delete.php?id=' .$row['id'].'"><button class="btn btn-danger btn-sm m-2" type="button">Delete</button></a>';
+       if(isset($_SESSION['adm'])){
+       $button = '<a href="update.php?id=' .$row['id'].'"><button class="btn btn-primary btn-sm m-2" type="button">Edit</button></a><a href="delete.php?id=' .$row['id'].'"><button class="btn btn-danger btn-sm m-2" type="button">Delete</button></a>';
 
-      // $button2 = '  <a href="create.php"><button class="btn btn-danger"type="button">Add product</button></a>';
-      // } else {
-      //   $button = '<a href=".php?id=' .$row['id'].'"><button class="btn btn-primary btn-sm m-2" type="button">Order</button></a>';
-      // }
+       $button2 = '  <a href="create.php"><button class="btn btn-danger"type="button">Add product</button></a>';
+       } else {
+         $button = '<a href=".php?id=' .$row['id'].'"><button class="btn btn-primary btn-sm m-2" type="button">Order</button></a>';
+       }
       
 
  $tbody .= '<div class="col">
@@ -34,8 +36,8 @@ if (mysqli_num_rows($result) > 0) {
                 <p class="card-text">'.$row["price"].'€</p>
                 <p class="card-text">'.$row["description"].'</p>
             </div>
-            <div class="d-flex justify-content-end">
-            <a href="update.php?id=' .$row['id'].'"><button class="btn btn-primary btn-sm m-2 ' . $class.'" type="button">Edit</button></a><a href="delete.php?id=' .$row['id'].'"><button class="btn btn-danger btn-sm m-2 ' . $class.'" type="button">Delete</button></a>
+            <div class="d-flex justify-content-end">'.$button.'
+           
            
             
            
@@ -63,58 +65,62 @@ mysqli_close($connect);
     <?php require_once "components/boot.php" ?>
 
     <style type="text/css">
-        *{
-            font-family: Georgia, serif;
-        }
-        .manageProduct {
-            margin: auto;
-        }
-        .manageProduct p {
-            text-align: center;
-            margin: 3vw;
-        }
-        .hide {
-          visibility: hidden;
-        }
-        .show {
-          visibility: visible;
-        }
+    * {
+        font-family: Georgia, serif;
+    }
 
-        .card:hover{
-            background-color: antiquewhite;
-        }
+    .manageProduct {
+        margin: auto;
+    }
 
+    .manageProduct p {
+        text-align: center;
+        margin: 3vw;
+    }
+
+    /* .hide {
+        visibility: hidden;
+    }
+
+    .show {
+        visibility: visible;
+    } */
+
+    .card:hover {
+        background-color: antiquewhite;
+    }
     </style>
 
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Delivery Italian</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<a href="login/logout.php?logout"><button class="btn btn-danger"type="button">Sign Out</button></a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Delivery Italian</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <a href="login/logout.php?logout"><button class="btn btn-danger" type="button">Sign Out</button></a>
     <div class="manageProduct w-75 mt-3">
         <p class='h2'>Menu</p>
         <div class="row row-cols-1 row-cols-md-3 g-4">
@@ -123,9 +129,10 @@ mysqli_close($connect);
 
         </div>
         <div class='mt-5'>
-        <a href="create.php"><button class="btn btn-danger <?= $class2 ?>" type="button">Add product</button></a>
-             
-            </div>
+            <?= $button2 ?>
+            <!-- <a href="create.php"><button class="btn btn-danger <?= $class2 ?>" type="button">Add product</button></a> -->
+
+        </div>
     </div>
 </body>
 
